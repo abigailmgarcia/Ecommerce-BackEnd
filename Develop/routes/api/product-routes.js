05841e14e41791048bd2b601f790315688bad2c8
abigailmgarcia <abigailmg31@gmail.com>
2023-07-30 21:59:50 -0500

    created get route for products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,10 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  const productData = await Product.findAll({
+    include: [
+      {model: Category},
+      {model: Tag}]
+  }).catch((err) => {
+    res.json(err)
+  });
+  res.json(productData)
+  });
 
 // get one product
 router.get('/:id', (req, res) => {
